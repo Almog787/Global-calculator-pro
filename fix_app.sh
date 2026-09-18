29f3398f6e680e67a592f1118607a9d9ff2aee6e
Almog787 <143160546+Almog787@users.noreply.github.com>
2026-09-18 09:09:12 +0300

    refactor: remove legacy build and maintenance scripts
    Cleaned up the repository by removing obsolete helper scripts used for temporary fixes, migrations, and linting adjustments. Also removed the unused `index-pages` script from `package.json`.

--- fix_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILES=("AgeCalculator" "BmiCalculator" "CompoundInterest" "MortgageCalculator" "PercentageFinder" "SalaryCalculator" "TipCalculator" "UnitConverter")
-
-for file in "${FILES[@]}"; do
-  sed -i "s/import('\.\/pages\/$file')/import('\.\/pages\/calculators\/$file')/g" src/App.tsx
-done
--- fix_syntax.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = 'src/contexts/i18n.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace("avec l'assistant", "avec l\\'assistant");
-fs.writeFileSync(file, content);
-console.log('fixed');
